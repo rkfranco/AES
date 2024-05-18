a7973595fabb622aaf1e3c56d3692b522ab8a487
Rodrigo Franco <rodrigokafra@hotmail.com>
2024-05-18 01:33:23 -0300

    [REFACTOR] Removendo StringWriter

--- src/aes/utils/BlockFilling.java
+++ src/aes/utils/BlockFilling.java
@@ -1,7 +1,5 @@
 package aes.utils;
 
-import java.io.StringWriter;
-
 public class BlockFilling {
     public static String pkcs7(String simpleText, int blockSize) {
         int qdtCharAppend = blockSize - simpleText.length() % blockSize;
@@ -11,16 +9,14 @@ public class BlockFilling {
     public static String pkcs7(int[] simpleText, int blockSize) {
         int qdtCharAppend = blockSize - simpleText.length % blockSize;
 
-        StringWriter sw = new StringWriter();
+        StringBuilder sb = new StringBuilder();
 
         for (int i : simpleText) {
-            sw.write(i);
+            sb.append((char) i);
         }
 
-        for (int i = 0; i < qdtCharAppend; i++) {
-            sw.write(qdtCharAppend);
-        }
+        sb.append(String.valueOf((char) qdtCharAppend).repeat(Math.max(0, qdtCharAppend)));
 
-        return sw.toString();
+        return sb.toString();
     }
 }
